d85cd7e5b6ac91e98c93985b5f07e189eb1c60e4
adbhanm <adbhanm@gmail.com>
2025-07-11 17:23:16 +0530

    ui

--- lib/calls.dart
+++ lib/calls.dart
@@ -4,6 +4,6 @@ class Calls extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(backgroundColor: Colors.pinkAccent,);
   }
 }
--- lib/community.dart
+++ lib/community.dart
@@ -4,6 +4,6 @@ class Community extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(backgroundColor: Colors.tealAccent,);
   }
 }
